01733c2c50b33992ab99e558acfb34e222cf34b5
berrondo <claudio.berrondo@gmail.com>
2021-07-19 02:39:46 -0300

    basic Event model with tested creation

--- ca/core/models.py
+++ ca/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    session_id = models.UUIDField()
+    category = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    data = models.JSONField()
+    timestamp = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
--- ca/core/tests.py
+++ ca/core/tests.py
@@ -1,3 +1,30 @@
+from uuid import UUID
 from django.test import TestCase
+from .models import Event
 
-# Create your tests here.
+
+class EventTestCase(TestCase):
+    def test_create_an_event(self):
+        # given
+        self.event = Event(
+            session_id="e2085be5-9137-4e4e-80b5-f1ffddc25423",
+            category="page interaction",
+            name="pageview",
+            data={
+                "host": "www.example.com",
+                "path": "/"
+            },
+            timestamp="2021-01-01 09:15:27.243860",
+        )
+        previous_count = Event.objects.count()
+
+        # when
+        self.event.save()
+
+        # then
+        new_count = Event.objects.count()
+        self.assertEqual(previous_count + 1, new_count)
+        self.assertEqual(
+            UUID("e2085be5-9137-4e4e-80b5-f1ffddc25423"),
+            Event.objects.last().session_id
+        )
