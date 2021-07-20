6be561473d48ece7c1c5b761ed94fb5170b1581a
berrondo <claudio.berrondo@gmail.com>
2021-07-19 21:42:12 -0300

    prevent updating through API: test fails

--- ca/core/views.py
+++ ca/core/views.py
@@ -1,9 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+
+from ca.core.models import Event
+from ca.core.serializers import EventSerializer
 
 from .models import Event
 from .serializers import EventSerializer
 
 
-class EventViewSet(viewsets.ModelViewSet):
+
+class EventViewSet(
+        mixins.CreateModelMixin,
+        mixins.RetrieveModelMixin,
+        mixins.ListModelMixin,
+        viewsets.GenericViewSet
+    ):
+
     queryset = Event.objects.all()
     serializer_class = EventSerializer
