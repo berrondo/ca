9828ce36153d265049b6892fd19da6c491ba583c
berrondo <claudio.berrondo@gmail.com>
2021-07-21 11:34:03 -0300

    introduces Event.data validations!

--- ca/core/management/commands/process_events.py
+++ ca/core/management/commands/process_events.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from ca.core.models import RawEvent
-from ca.core.serializers.event_serializers import EventSerializer
+from ca.core.serializers.data_serializers import EventAndDataserializer
 
 
 class Command(BaseCommand):
@@ -10,15 +10,20 @@ class Command(BaseCommand):
         raw_events = RawEvent.objects.filter(status=RawEvent.Status.RECEIVED)
 
         for raw_event in raw_events:
-            data = raw_event.payload
-            serializer = EventSerializer(data=data)
+            serializer = EventAndDataserializer(data=raw_event.payload)
+
             if serializer.is_valid():
-                serializer.save()
-                raw_event.status = RawEvent.Status.PROCESSED
-                raw_event.save()
-                self.stdout.write(self.style.SUCCESS('Successfully processed Event "%s"' % raw_event.pk))
-            else:
-                self.stdout.write(self.style.ERROR(raw_event.payload))
-                self.stdout.write(self.style.ERROR(serializer.errors))
-                raw_event.status = RawEvent.Status.INVALID
-                raw_event.save()
+                if serializer.event_type_data_is_valid():
+                    serializer.save()
+                    raw_event.status = RawEvent.Status.PROCESSED
+                    raw_event.save()
+                    self.stdout.write(self.style.SUCCESS('Successfully processed Event "%s"' % raw_event.pk))
+                    continue
+
+                else:
+                    serializer = serializer.instance_event_type_data_serializer
+
+            self.stdout.write(self.style.ERROR(raw_event.payload))
+            self.stdout.write(self.style.ERROR(serializer.errors))
+            raw_event.status = RawEvent.Status.INVALID
+            raw_event.save()
---
+++ ca/core/serializers/data_serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+
+from ca.core.serializers.event_serializers import EventSerializer
+
+
+class EventAndDataserializer(EventSerializer):
+    @property
+    def event_type(self):
+        if event := self.initial_data:
+            if category := event.get('category'):
+                if name := event.get('name'):
+                    return " ".join((category, name))
+        return ""
+
+    @property
+    def event_type_data(self):
+        if event := self.initial_data:
+            if data := event.get('data'):
+                return data
+        return ""
+
+    @property
+    def event_type_data_serializer(self):
+        return _event_type_serializer[self.event_type]
+
+    def event_type_data_is_valid(self):
+        self.instance_event_type_data_serializer = self.event_type_data_serializer(data=self.event_type_data)
+        return self.instance_event_type_data_serializer.is_valid()
+
+
+class PageViewSerializer(serializers.Serializer):
+    host = serializers.CharField()
+    path = serializers.CharField()
+
+
+class PageClickSerializer(PageViewSerializer):
+    element = serializers.CharField()
+
+
+class FormSubmitSerializer(PageViewSerializer):
+    form = serializers.JSONField()
+
+
+_event_type_serializer = {
+    "page interaction pageview": PageViewSerializer,
+    "page interaction cta click": PageClickSerializer,
+    "form interaction submit": FormSubmitSerializer,
+}
