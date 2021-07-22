c461f4d37f4bf4575a65e2f6d95ca7f062466656
berrondo <claudio.berrondo@gmail.com>
2021-07-21 21:29:02 -0300

    installing the example client

--- ca/urls.py
+++ ca/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from .core import views
+from ca.core import views
 
 router = routers.DefaultRouter()
 router.register(r"events", views.EventViewSet)
@@ -11,4 +11,5 @@ router.register(r"raw-events", views.RawEventViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path('admin/', admin.site.urls),
+    path('example/', include('ca.example.urls')),
 ]
